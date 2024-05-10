828145714fadcf16224d953acdcc717c889f9ee3
asus test <msa-99@outlook.com>
2024-05-10 10:20:31 +0600

    problem solving FinalEExam_1_C

---
+++ tigerVSPathan.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<limits.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+    int testCase;
+    scanf("%d",&testCase);
+    while(testCase--){
+        int len;
+        scanf("%d",&len);
+        char s[len];
+    scanf("%s",&s);
+    int tiger=0, pathaan=0;
+    for(int i=0; i<len; i++){
+        if(s[i]=='T'){
+            tiger++;
+        }else if(s[i]=='P'){
+            pathaan++;
+        }
+    }
+    if(pathaan>tiger){
+        printf("Pathaan");
+    }else if(tiger>pathaan){
+        printf("Tiger");
+    }else{
+        printf("Draw");
+    }
+    printf("\n");
+    }
+    
+    return 0;
+}
